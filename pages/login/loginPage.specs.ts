dd900f904321249cecfa5160e53dfc1640ea2844
KuzminAntton <anttonkuzmin@gmail.com>
2023-10-09 13:36:10 +0200

    added tests related on verification for no logged user to verify that Social tab is available for not logged user and Explore and Chat is not and should redirect user to login page

--- pages/login/loginPage.specs.ts
+++ pages/login/loginPage.specs.ts
@@ -2,13 +2,13 @@ import type { Page, Locator } from '@playwright/test';
 import {expect} from "@playwright/test";
 
 export class LoginPage {
-    private readonly idOfThePage: Locator;
+    private readonly IDOfThePage: Locator;
     private readonly loginField: Locator;
     private readonly passwordField: Locator;
     private readonly submitButton: Locator;
 
     constructor(readonly page : Page) {
-        this.idOfThePage = this.page.locator('css=#login_cont');
+        this.IDOfThePage = this.page.locator('css=#login_cont');
         this.loginField = this.page.locator('css=#dv_email_usr');
         this.passwordField = this.page.locator('xpath=//input[@type="password"]');
         this.submitButton = this.page.locator('css=#logIn1');
@@ -22,6 +22,6 @@ export class LoginPage {
     }
 
     async verifyThatIsLoginPage() {
-        await expect(this.idOfThePage).toBeVisible();
+        await expect(this.IDOfThePage).toBeVisible();
     }
 }
